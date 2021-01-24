735fb578244a19ee1e75448ec1cf8d7183e5d198
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-24 18:16:16 +0100

    removed unused values

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useRoutes, A } from 'hookrouter'
 
-import Link from '@material-ui/core/Link'
-
 import routes from './Routes/Routes'
 
 import Signup from './components/signup/Signup'
 import Login from './components/login/Login'
 import Navbar from './components/navbar/Navbar'
 import Landing from './components/landing/Landing'
+import { Profile } from './components/Profile';
 
 function App() {
   const routeResult = useRoutes(routes)
@@ -21,11 +20,11 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           <Route path="/" component={Landing} />
-          {/* <Route path="/" component={Landing} /> */}
+          <Route path="/profile" component={Profile} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
